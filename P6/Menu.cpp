162ede2a6ffcd95c4234e3622f378bfa69feb030
Kevin <keviniza64@gmail.com>
2020-12-13 01:19:10 -0500

    Project 6 Completed

--- P6/Menu.cpp
+++ P6/Menu.cpp
@@ -27,7 +27,7 @@ void Menu::Render(ShaderProgram* program) {
     program->SetViewMatrix(state.uiViewMatrix);
 
     Util::DrawText(program, state.fontTextureID, "ONLY SURVIVE", 1, -0.3f, glm::vec3(-3.5, 2, 0));
-    Util::DrawText(program, state.fontTextureID, "You have 3 lives, you're mission is to survive all 12 rounds", 0.5f, -0.3f, glm::vec3(-6, 0, 0));
+    Util::DrawText(program, state.fontTextureID, "You have 3 lives, your mission is to survive all 12 rounds", 0.5f, -0.3f, glm::vec3(-6, 0, 0));
     Util::DrawText(program, state.fontTextureID, " press space to shoot, AWSD to move", 0.5f, -0.3f, glm::vec3(-3, -0.5f, 0));
     Util::DrawText(program, state.fontTextureID, "Press Enter to Start", 0.75f, -0.4f, glm::vec3(-3, -2, 0));
 }
